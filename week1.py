7ed4a3246ce001433a41429840f294b0e7dd064f
Ahdiani Febriyanti <z5507587@ad.unsw.edu.au>
2024-07-27 12:21:07 +1000

    Initial commit

---
+++ week1.py
@@ -0,0 +1,14 @@
+# print('Welcome to the Toolkit for Finance T2 2024, hello from Ahdiani Febriyanti')
+
+# Downloads Qantas share price beginning 1 January 2020
+import yfinance                                           # (1)
+tic = "QAN.AX"                                            # (2)
+start = '2020-01-01'                                      # (3)
+end = None                                                # (4)
+df = yfinance.download(tic, start, end, ignore_tz=True)   # (5)
+print(df)                                                 # (6)
+df.to_csv('qan_stk_prc.csv')                              # (7)
+
+# x= int (1)
+
+y = 1+2
